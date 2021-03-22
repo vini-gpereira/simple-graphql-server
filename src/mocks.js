77353e32216b5602b799b10d1bba071792336be5
Vinicius Pereira <vini.gpereira2014@gmail.com>
2021-03-22 18:22:52 -0300

    chore: remove unused uuid dependency

--- src/mocks.js
+++ src/mocks.js
@@ -1,22 +1,13 @@
-const { v4: uuidv4 } = require('uuid');
-
-const userId1 = uuidv4();
-const userId2 = uuidv4();
-const userId3 = uuidv4();
-const userId4 = uuidv4();
-const postId1 = uuidv4();
-const postId2 = uuidv4();
-
 const users = [
-    { _id: userId1, name: 'Vinicius', email: 'vinicius@email.com', posts: [postId1] },
-    { _id: userId2, name: 'Gabriel', email: 'gabriel@email.com', posts: [] },
-    { _id: userId3, name: 'Regis', email: 'regis@email.com', posts: [] },
-    { _id: userId4, name: 'Rafael', email: 'rafael@email.com', posts: [postId2] },
+    { _id: "0", name: 'Vinicius', email: 'vinicius@email.com', posts: ["0"] },
+    { _id: "1", name: 'Gabriel', email: 'gabriel@email.com', posts: [] },
+    { _id: "2", name: 'Regis', email: 'regis@email.com', posts: [] },
+    { _id: "3", name: 'Rafael', email: 'rafael@email.com', posts: ["1"] },
 ];
 
 const posts = [
-    { _id: postId1, title: 'Receita de Bolo', content: 'Para começar o bolo...', author: userId1 },
-    { _id: postId2, title: 'Tutorial', content: 'Neste tutorial, vou ensinar...', author: userId4 },
+    { _id: "0", title: 'Receita de Bolo', content: 'Para começar o bolo...', author: "0" },
+    { _id: "1", title: 'Tutorial', content: 'Neste tutorial, vou ensinar...', author: "3" },
 ];
 
 module.exports = {
